26ca75bf19448cdff50f27501ced8fef7208b01b
tonydle <tony@mechatony.com>
2020-08-05 01:13:00 +1000

    🌏 Added UR3 World with first person camera

---
+++ unity/UR3Sim/Assets/Addons/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+
+
+    void Update()
+    {
+        velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+    }
+}
