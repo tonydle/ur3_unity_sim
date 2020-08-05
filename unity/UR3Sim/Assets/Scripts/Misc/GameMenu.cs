15d56b3eb7e6377f5b8ed994c77b6f63059cc4c7
tonydle <tony@mechatony.com>
2020-08-05 16:00:33 +1000

    🛠️ Added Main Menu with ROS IP option

---
+++ unity/UR3Sim/Assets/Scripts/Misc/GameMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject inputField;
+    private void Awake() {
+        string lastIP = PlayerPrefs.GetString("ROSIP", "192.168.2.2");
+        inputField.GetComponent<TMP_InputField>().text = lastIP;
+    }
+
+    public void StartSim()
+    {
+        SceneManager.LoadScene("UR3 World");
+    }
+
+    public void QuitSim()
+    {
+        Debug.Log("Application is quitting ...");
+        Application.Quit();
+    }
+
+    public void ChangeIP()
+    {
+        string ip = inputField.GetComponent<TMP_InputField>().text;
+        PlayerPrefs.SetString("ROSIP", ip);
+    }
+}
---
+++ unity/UR3Sim/Assets/Scripts/Misc/SetIP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class SetIP : MonoBehaviour
+    {
+        private void Awake() {
+            RosConnector rosConnector = GetComponent<RosConnector>();
+            string ip = PlayerPrefs.GetString("ROSIP", "192.168.2.2");
+            string fullIP = "ws://" + ip + ":9090";
+            rosConnector.RosBridgeServerUrl = fullIP;
+        }
+    }
+}
